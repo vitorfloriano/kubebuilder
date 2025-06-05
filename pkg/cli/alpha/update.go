71756bf01a8bb34bc5e75780266ea0fe1deede81
Vitor Floriano <107767584+vitorfloriano@users.noreply.github.com>
2025-06-05 01:10:42 -0300

    Add new alpha update command (#8)
    This commit adds the new alpha update command. It's a WIP.

---
+++ pkg/cli/alpha/update.go
@@ -0,0 +1,29 @@
+package alpha
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"sigs.k8s.io/kubebuilder/v4/pkg/cli/alpha/internal"
+)
+
+func NewUpdateCommand() *cobra.Command {
+	opts := internal.Update{}
+	updateCmd := &cobra.Command{
+		Use:   "update",
+		Short: "TODO: add a short description of the command",
+		Long: `TODO: add a long description of the command.
+Provide usage examples.`,
+		// 	TODO: add validation here
+		//	PreRunE: func(_ *cobra.Command, _ []string) error {
+		//		return opts.Validate()
+		//	},
+		Run: func(_ *cobra.Command, _ []string) {
+			if err := opts.Update(); err != nil {
+				log.Fatalf("Failed to execute: %s", err)
+			}
+		},
+	}
+	// TODO: add flags here later
+
+	return updateCmd
+}
