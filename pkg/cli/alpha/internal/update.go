2f2832095a748b4da27c87dcade802f721744f43
Vitor <107767584+vitorfloriano@users.noreply.github.com>
2025-06-05 12:14:23 -0300

    Add logic for loading and getting the cliVersion from PROJECT This commit implements the yaml store for loading the PROJECT config file and adds the cliVersion variable to hold the cliVersion field extracted using the GetCliVersion() function already present in the codebase.

--- pkg/cli/alpha/internal/update.go
+++ pkg/cli/alpha/internal/update.go
@@ -2,16 +2,34 @@ package internal
 
 import (
 	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
+	"sigs.k8s.io/kubebuilder/v4/pkg/config/store/yaml"
+	"sigs.k8s.io/kubebuilder/v4/pkg/machinery"
 )
 
 type Update struct {
 	// TODO: populate the struct
-	Field string
+	FromVersion string
 }
 
 func (opts *Update) Update() error {
 	// TODO: add implementation logic
 	fmt.Println("WIP...")
+
+	projectConfigFile := yaml.New(machinery.Filesystem{FS: afero.NewOsFs()})
+	if err := projectConfigFile.LoadFrom(yaml.DefaultPath); err != nil { // TODO: assess if DefaultPath could be renamed to ConfigFilePath
+		return fmt.Errorf("TODO: add error message: %w", err)
+	}
+
+	cliVersion := projectConfigFile.Config().GetCliVersion()
+
+	// TODO: add logic for overriding the cliVersion from the PROJECT file with the
+	// value passed to the --from-version flag just so that projects prior to 4.6.0
+	// can use it.
+
+	log.Infof("TODO: add message for CLI version being used: %s", cliVersion)
 
 	return nil
 }
